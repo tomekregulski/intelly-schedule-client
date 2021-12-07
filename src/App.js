1404d3b8a0b35dd129f3360fb2ccd9530b26b453
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-12-07 15:53:33 -0500

    Create placeholder table with sorting functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Table from './components/Table';
 
 function App() {
+  const array = ['mozzarella', 'gouda', 'cheddar'];
+
   return (
     <div className='App'>
       <h1>Schedule</h1>
+      <Table products={array} />
     </div>
   );
 }
